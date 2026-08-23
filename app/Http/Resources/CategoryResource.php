b3db59d9055edf1b3b0db95de0abb968b10ab292
beqa21beraia <beka.beraia.2@btu.edu.ge>
2026-08-23 23:59:12 +0400

    fix: correct CategoryResource fields

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -7,21 +7,14 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class CategoryResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'meta_keys' => $this->meta_keys,
-            'meta_description' => $this->meta_description,
-            'parent_id' => $this->parent_id,
             'route' => $this->route,
             'position' => $this->position,
+            'children' => CategoryResource::collection($this->whenLoaded('children'))
         ];
     }
 }
