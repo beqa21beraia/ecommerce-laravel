9483b3d13b2b22ef5a138062187ea8a4fcb7128c
beqa21beraia <beka.beraia.2@btu.edu.ge>
2026-09-15 11:29:34 +0400

    feat: install AdminLTE and add sessions table migration

---
+++ resources/views/admin/test.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Admin Test')
+
+@section('content_header')
+    <h1>It works</h1>
+@endsection
+
+@section('content')
+    <p>AdminLTE is wired up correctly.</p>
+@endsection
